86f8c79d230cda878856d72cdc18e19187f016fc
Patrik Majer <patrik.majer.pisek@gmail.com>
2020-11-12 17:30:28 +0100

    using owner in CRD-ipam for IPa reservation for specific pod

--- pkg/ipam/crd.go
+++ pkg/ipam/crd.go
@@ -383,12 +383,42 @@ func (n *nodeStore) allocate(ip net.IP) (*ipamTypes.AllocationIP, error) {
 }
 
 // allocateNext allocates the next available IP or returns an error
-func (n *nodeStore) allocateNext(allocated ipamTypes.AllocationMap, family Family) (net.IP, *ipamTypes.AllocationIP, error) {
+func (n *nodeStore) allocateNext(allocated ipamTypes.AllocationMap, family Family, owner string) (net.IP, *ipamTypes.AllocationIP, error) {
 	n.mutex.RLock()
 	defer n.mutex.RUnlock()
 
 	if n.ownNode == nil {
 		return nil, nil, fmt.Errorf("CiliumNode for own node is not available")
+	}
+
+	// first we check reservation
+	for ip, ipInfo := range n.ownNode.Spec.IPAM.Pool {
+		if _, ok := allocated[ip]; !ok {
+			parsedIP := net.ParseIP(ip)
+			if parsedIP == nil {
+				log.WithFields(logrus.Fields{
+					fieldName: n.ownNode.Name,
+					"ip":      ip,
+				}).Warning("Unable to parse IP in CiliumNode custom resource")
+				continue
+			}
+
+			if DeriveFamily(parsedIP) != family {
+				continue
+			}
+
+			if ipInfo.Owner != "" {
+				log.WithFields(logrus.Fields{
+					"ip":            ip,
+					"requiredOwner": ipInfo.Owner,
+					"realOwner":     owner,
+				}).Info("Parsed IP address in CiliumNode")
+
+				if ipInfo.Owner == owner {
+					return parsedIP, &ipInfo, nil
+				}
+			}
+		}
 	}
 
 	// FIXME: This is currently using a brute-force method that can be
@@ -595,7 +625,7 @@ func (a *crdAllocator) AllocateNext(owner string) (*AllocationResult, error) {
 	a.mutex.Lock()
 	defer a.mutex.Unlock()
 
-	ip, ipInfo, err := a.store.allocateNext(a.allocated, a.family)
+	ip, ipInfo, err := a.store.allocateNext(a.allocated, a.family, owner)
 	if err != nil {
 		return nil, err
 	}
@@ -614,7 +644,7 @@ func (a *crdAllocator) AllocateNextWithoutSyncUpstream(owner string) (*Allocatio
 	a.mutex.Lock()
 	defer a.mutex.Unlock()
 
-	ip, ipInfo, err := a.store.allocateNext(a.allocated, a.family)
+	ip, ipInfo, err := a.store.allocateNext(a.allocated, a.family, owner)
 	if err != nil {
 		return nil, err
 	}
